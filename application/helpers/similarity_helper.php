a8b5db427e93368574f72744d8149349d9508a5d
her0satr <her0satr@yahoo.com>
2013-02-25 17:41:56 -0800

    First Master

---
+++ application/helpers/similarity_helper.php
@@ -0,0 +1,61 @@
+<?php
+if (! class_exists('Similarity')) {
+	class Similarity {
+		function __construct($Primary, $Secondary) {
+			$this->Result = 0;
+			$this->Primary = $Primary;
+			$this->Secondary = $Secondary;
+			
+			$this->Proccess();
+		}
+		
+		function Proccess() {
+			$ArrayRate = array(0, 0);
+			$this->ArrayMerge = array();
+			foreach ($this->Primary as $Key => $Value) {
+				if (isset($this->Primary[$Key]) && isset($this->Secondary[$Key])) {
+					$ArrayRate[0] += $this->Primary[$Key];
+					$ArrayRate[1] += $this->Secondary[$Key];
+					$this->ArrayMerge[] = array( $this->Primary[$Key], $this->Secondary[$Key] );
+				}
+			}
+			
+			// Get User Count and return if no user set a rating
+			$CountUser = count($this->ArrayMerge);
+			if (empty($CountUser)) {
+				return;
+			}
+			
+			
+			// Get Final Rate
+			foreach ($ArrayRate as $Key => $Value) {
+				$this->ArrayRate[$Key] = number_format($Value / $CountUser, 2, '.', '');
+			}
+			
+			
+			#region Similarity
+			#Make it two proccess to make is easier to learn
+			
+			// Get Upper Value
+			$UpperValue = 0;
+			foreach ($this->ArrayMerge as $Array) {
+				$UpperValue += ($Array[0] - $this->ArrayRate[0]) * ($Array[1] - $this->ArrayRate[1]);
+			}
+			
+			// Get Lower Value
+			$LeftSite = $RightSite = $LowerValue = 0;
+			foreach ($this->ArrayMerge as $Array) {
+				$LeftSite += pow($Array[0] - $this->ArrayRate[0], 2);
+				$RightSite += pow($Array[1] - $this->ArrayRate[1], 2);
+			}
+			$LowerValue = sqrt($LeftSite) * sqrt($RightSite);
+			
+			#endregion Similarity
+			
+			if (!empty($UpperValue) && !empty($LowerValue)) {
+				$this->Result = $UpperValue / $LowerValue;
+			}
+		}
+	}
+}
+?>
